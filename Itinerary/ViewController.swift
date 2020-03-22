326b757e3a3a2520662dbfc02c0a528e35a752df
Bryan Tong <bryantong1257@gmail.com>
2020-03-21 22:34:07 -0400

    Initial Commit

---
+++ Itinerary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Itinerary
+//
+//  Created by Bryan  on 3/21/20.
+//  Copyright © 2020 Bryan Tong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
