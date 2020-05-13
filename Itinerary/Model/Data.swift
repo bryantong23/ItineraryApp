798d01b7983a001b58c900ce3d0069b81645825e
Bryan Tong <bryantong1257@gmail.com>
2020-05-13 14:25:05 -0400

    added readTrips code

---
+++ Itinerary/Model/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  Itinerary
+//
+//  Created by Bryan  on 5/13/20.
+//  Copyright © 2020 Bryan Tong. All rights reserved.
+//
+
+import Foundation
+
+class Data {
+    static var tripModels = [TripModel]()
+    
+}
